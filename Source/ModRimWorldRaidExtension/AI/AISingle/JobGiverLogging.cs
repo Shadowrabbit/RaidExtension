d92af82b929c7ad9d7db0cb06fd747a7ce3a4905
yingtu0401@gmail.com <yingtu0401@gmail.com>
2021-06-19 18:47:19 +0800

    偷猎事件分离

---
+++ Source/ModRimWorldRaidExtension/AI/AISingle/JobGiverLogging.cs
@@ -0,0 +1,16 @@
+﻿// ******************************************************************
+//       /\ /|       @file       JobGiverLogging.cs
+//       \ V/        @brief      行为节点 伐木
+//       | "")       @author     Shadowrabbit, yingtu0401@gmail.com
+//       /  |                    
+//      /  \\        @Modified   2021-06-19 12:00:02
+//    *(__\_\        @Copyright  Copyright (c) 2021, Shadowrabbit
+// ******************************************************************
+
+namespace SR.ModRimWorld.FactionalWar
+{
+    public class JobGiverLogging
+    {
+        
+    }
+}
